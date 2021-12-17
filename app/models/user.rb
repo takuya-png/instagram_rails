77f7f1ead5371d96d00a592a7f4654898f0d5a6c
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-17 10:45:37 +0900

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :pictures
   has_many :favorites, dependent: :destroy
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  before_validation { email.downcase! }
+  has_secure_password
+  mount_uploader :image, ImageUploader
 end
