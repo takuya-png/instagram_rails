77f1e5398d88e2d13f654bf3cb4e5c5bcfec7561
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-17 14:10:53 +0900

    pictureモデルに文字数制限等追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
+  validates :title, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 1000}
 end
