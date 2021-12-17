0cd60c6a74711f8d8d5e8c1718eac98a03364d34
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-17 19:39:51 +0900

    pictureコントローラ修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,28 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :set_picture, only: %i[ show edit update destroy ]
 
   def index
     @pictures = Picture.all
   end
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
   end
 
   def create
-    @picture = Picture.new(picture_params)
-    @picture.user_id = current_user.:id
-    if params[:back]
-      render :new
-    else
+    @picture = current_user.pictures.build(picture_params)
+    respond_to do |format|
       if @picture.save
-        PictureMailer.picture_mail(@picture).deliver
-        redirect_to pictures_path, notice: "ブログを作成しました"
+        ConfirmMailer.confirm_mail(@picture).deliver
+        format.html { redirect_to @picture, notice: "Picture was successfully created." }
+        format.json { render :show, status: :created, location: @picture }
       else
-        render :new
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -29,33 +32,42 @@ class PicturesController < ApplicationController
   end
 
   def edit
+    if @picture.id != current_user.id
+      redirect_to pictures_path
+    end
   end
 
   def update
-    if @picture.update(picture_params)
-      redirect_to pictures_path, notice: "ブログを編集しました！"
-    else
-      render :edit
+    respond_to do |format|
+      if @picture.update(picture_params)
+        format.html { redirect_to @picture, notice: "Picture was successfully updated." }
+        format.json { render :show, status: :ok, location: @picture }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @picture.destroy
-    redirect_to pictures_path, notice: "ブログを削除しました！"
+    respond_to do |format|
+      format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   def confirm
-    @picutre = Picture.new(picture_params)
-    @picture.user_id = current_user.id
+    @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
   end
 
   private
-  def picture_params
-    params.require(:picture).permit(:title, :content, :image, :image_cache)
-  end
-
   def set_picture
     @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image, :image_cache, :user_id)
   end
 end
