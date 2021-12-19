41f81b063abeb5ae61c83cd722d3703aaee44e03
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-19 16:26:25 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root 'pictures#new'
-  resources :favorites, only: [:index, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  root 'pictures#index'
   resources :pictures do
     collection do
       post :confirm
     end
   end
+  resources :favorites, only: [:index, :create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
